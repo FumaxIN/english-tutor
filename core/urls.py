afd159e8db7992e9fdc72a337823743dbc64296a
fumaxin <kalpeshdhoundiyal@gmail.com>
2024-10-02 10:34:38 +0530

    structure setup

---
+++ core/urls.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django.views import defaults as default_views
+from django.views.generic.base import RedirectView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+
+urlpatterns = [
+    path(settings.ADMIN_URL, admin.site.urls),
+]
+
+# API URLs
+api_urlpatterns = [
+    # API base URL
+    # path("api/", include("tutor.api_router")),
+    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
+]
+
+if settings.DEBUG:
+    api_urlpatterns += [
+        path(
+            "api/schema/swagger/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+        path("", RedirectView.as_view(url="/api/schema/swagger/", permanent=False)),
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
+
+urlpatterns += api_urlpatterns
