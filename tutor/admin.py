5a3524778f00826500c4f822fd1931a1dffd4d1b
fumaxin <kalpeshdhoundiyal@gmail.com>
2024-10-02 15:43:40 +0530

    admin config

--- tutor/admin.py
+++ tutor/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from djangoql.admin import DjangoQLSearchMixin
 
-# Register your models here.
+from tutor.models import Error, User
+
+@admin.register(User)
+class UserAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    pass
+
+@admin.register(Error)
+class ErrorAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    pass
+
+
--- tutor/models/errors.py
+++ tutor/models/errors.py
@@ -3,18 +3,18 @@ from django.db import models
 from django.utils.timezone import now
 
 from .users import User
-from .enums import SuperBucket
+from .enums import SuperBucket, SubBucket
 
 
 class Error(models.Model):
     id = uuid.uuid4()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     errorCategory = models.IntegerField(choices=SuperBucket.choices)
-    errorSubCategory = models.IntegerField()
+    errorSubCategory = models.IntegerField(choices=SubBucket.choices)
     timestamp_utc = models.DateTimeField(default=now)
 
     def __str__(self):
-        return f"{self.user} - {self.errorCategory} | {self.errorSubCategory} | {self.timestamp_utc}"
+        return f"{self.user.username} - {SuperBucket(self.errorCategory).name} - {SubBucket(self.errorSubCategory).name}"
 
     class Meta:
         verbose_name_plural = "Errors"
